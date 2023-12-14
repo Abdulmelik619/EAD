73ba97a9a65c0e7e5e40173ca6b85a214734a053
Abdulmelik619 <111171040+Abdulmelik619@users.noreply.github.com>
2023-12-14 13:01:15 +0100

    Delete EAD-TaskManagmentSystem directory

--- EAD-TaskManagmentSystem/src/task/db_config.java
+++
@@ -1,7 +0,0 @@
-package task;
-
-public class db_config {
-	public static final String DB_URL = "jdbc:mysql://localhost:3306/users";
-	public static final String DB_USER = "root";
-	public static final String DB_PASSWORD = "";
-}
